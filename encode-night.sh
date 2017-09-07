53d94fe48f31e76bea6ae50e9468e001c8edf172
Mark Raynsford <code@io7m.com>
2017-09-07 17:39:40 +0000

    Encode day/night scripts were incorrectly named

--- encode-night.sh
+++ encode-night.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec ffmpeg -f image2 -i chemriver/%04d.png -c:v libx264 -crf 0 -preset veryslow chemriver.mkv
+exec ffmpeg -f image2 -i chemriver-night/%04d.png -r 30 -c:v libx264 -crf 0 -preset veryslow chemriver-night.mkv

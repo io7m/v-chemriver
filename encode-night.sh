c38a41132765db4128e2e5170df9b15581b689de
Mark Raynsford <code@io7m.com>
2017-09-08 09:02:59 +0000

    Attempt to correct framerates in encoding scripts

--- encode-night.sh
+++ encode-night.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec ffmpeg -f image2 -i chemriver-night/%04d.png -framerate 30 -r 30 -c:v libx264 -crf 0 -preset veryslow chemriver-night.mkv
+exec ffmpeg -f image2 -framerate 30 -i chemriver-night/%04d.png -c:v libx264 -crf 0 -preset veryslow chemriver-night.mkv
